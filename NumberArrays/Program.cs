a3ea7e7462b4ec93a845e0d32f6fc77831317a60
Kristjan Palm <kpalm@tktk.ee>
2023-03-10 11:01:35 +0200

    add week6 code

---
+++ NumberArrays/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+int[] numbers = new int[10];
+Random rnd = new Random();
+
+int sum = 0;    
+
+//populating the Array
+for (int i = 0;i < numbers.Length; i++)
+{
+    numbers[i] = rnd.Next(1, numbers.Length + 1);
+    sum = sum + numbers[i]; 
+}
+foreach (int number in numbers)
+{
+    Console.WriteLine(number);
+}
+ Console.WriteLine($"total: {sum}");
